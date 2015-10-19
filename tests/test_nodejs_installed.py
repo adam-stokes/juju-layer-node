1cbb242a129f1825a25859a11b3eab60105cb1cf
Adam Stokes <adam.stokes@ubuntu.com>
2015-10-18 22:52:45 -0400

    add tests
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ tests/test_nodejs_installed.py
@@ -0,0 +1,16 @@
+from charmhelpers.core import hookenv
+import os
+
+config = hookenv.config()
+
+
+class TestNodeJSInstalled:
+    def test_node_is_installed(self):
+        """ Test to make sure node.js was installed
+        """
+        assert os.path.isfile('/usr/bin/node')
+
+    def test_npm_is_installed(self):
+        """ Test to make sure npm was installed
+        """
+        assert os.path.isfile('/usr/bin/npm')
