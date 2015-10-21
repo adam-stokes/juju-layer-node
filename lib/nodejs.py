13069102e2262ae09d9f174a3de1813b763dbd64
Adam Stokes <adam.stokes@ubuntu.com>
2015-10-21 18:54:16 -0400

    rely on the routine to halt the workload
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/nodejs.py
+++ lib/nodejs.py
@@ -1,5 +1,6 @@
 import shlex
 import os
+import sys
 from collections import deque
 import subprocess
 
@@ -8,6 +9,9 @@ from charmhelpers.core import hookenv
 
 def node_dist_dir():
     """ Absolute path of Node.js application dir
+
+    Returns:
+    Absolute string of node application directory
     """
     config = hookenv.config()
     return os.path.join(hookenv.charm_dir(), config['node-application-dir'])
@@ -19,10 +23,25 @@ def npm(cmd):
     This layer relies on the use of npm scripts defined in `package.json`,
     see here https://docs.npmjs.com/misc/scripts for more information.
 
-    :param str cmd: Command to run can be string or list
-    :rtype bool: Returns True/False of npm execution status
+    Usage:
+
+       npm('install')
+       npm('run build')
+
+    Arguments:
+    cmd: Command to run can be string or list
+
+    Returns:
+    Will halt on error
     """
+    hookenv.status_set('maintenance', 'Installing NPM dependencies')
+    os.chdir(node_dist_dir())
     if isinstance(cmd, str):
         cmd = deque(shlex.split(cmd))
     cmd.appendleft('npm')
-    return subprocess.check_call(cmd) == 0
+    try:
+        subprocess.check_call(cmd)
+        os.chdir(os.getenv('CHARM_DIR'))
+    except subprocess.CalledProcessError as e:
+        hookenv.status_set("blocked", "NPM error: {}".format(e))
+        sys.exit(1)
